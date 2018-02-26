4b9e873b431e4a83921ee0e4f7473e52d98ca261
Benoît Ripoche <ripoche.b@gmail.com>
2018-02-26 13:21:42 +0100

    add unity files

---
+++ unity/Assets/GetApiData.cs
@@ -0,0 +1,46 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetApiData : MonoBehaviour {
+
+	// Set the url where you get the data
+	private string url = "http://localhost:9999/";
+
+	// set the interval for fetching data
+	private float interval = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetData(interval));
+	}
+
+	// Update is called once per frame
+	void Update () {}
+
+	private IEnumerator GetData(float waitTime) {
+		while (true)
+		{
+			// wait
+			yield return new WaitForSeconds(waitTime);
+			Debug.Log("Get Data");
+
+			WWW www = new WWW(url);
+
+			while(!www.isDone && string.IsNullOrEmpty(www.error)) {
+				//this.GetComponent<TextMesh>().text = "Loading..."; //Show progress
+				yield return null;
+			}
+
+			Data data = JsonUtility.FromJson<Data>(www.text);
+
+			if(string.IsNullOrEmpty(www.error)) this.GetComponent<TextMesh>().text = data.date;
+		}
+	}
+}
+
+
+public class Data
+{
+	public string date;
+}
---
+++ unity/Assets/GetWebcam.cs
@@ -0,0 +1,24 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetWebcam : MonoBehaviour {
+
+	static WebCamTexture backCam;
+
+	// Use this for initialization
+	void Start () {
+		if (backCam == null)
+			backCam = new WebCamTexture();
+
+		GetComponent<Renderer>().material.mainTexture = backCam;
+
+		if (!backCam.isPlaying)
+			backCam.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
